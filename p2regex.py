aad240006b6b6d0673d2d4507d1849c9f7906590
Mate2001 <136878792+Mate2001@users.noreply.github.com>
2023-06-28 22:36:27 +0200

    Delete p2regex.py

--- p2regex.py
+++
@@ -1,9 +0,0 @@
-import re
-
-regex = r"^[K][a-zA-Z]*[0-5].*[Razmak][a-zA-Z]*[S]$"
-string = "Kristijan Sliskovic"
-
-if re.match(regex, string):
-    print("Podudaranje pronađeno!")
-else:
-    print("Podudaranje nije pronađeno.")
